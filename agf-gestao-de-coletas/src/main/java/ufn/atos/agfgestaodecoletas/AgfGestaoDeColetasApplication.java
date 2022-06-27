98fd84f6d956da1011aff86a71844e06c329c90e
kaduvizu <94651752+CarlosVizu@users.noreply.github.com>
2022-06-27 16:26:57 -0300

    Criação do projeto via Springboot

---
+++ agf-gestao-de-coletas/src/main/java/ufn/atos/agfgestaodecoletas/AgfGestaoDeColetasApplication.java
@@ -0,0 +1,13 @@
+package ufn.atos.agfgestaodecoletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgfGestaoDeColetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgfGestaoDeColetasApplication.class, args);
+	}
+
+}
---
+++ agf-gestao-de-coletas/src/test/java/ufn/atos/agfgestaodecoletas/AgfGestaoDeColetasApplicationTests.java
@@ -0,0 +1,13 @@
+package ufn.atos.agfgestaodecoletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgfGestaoDeColetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
